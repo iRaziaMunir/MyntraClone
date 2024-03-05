e096d02dec61eee46908cc146fb426efdddaa3ad
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-03-05 10:19:46 +0500

    frontend of home page is almost  completed

---
+++ Myntra.js
@@ -0,0 +1,15 @@
+let bagButton = doccument.querySelector('.bag-btn');
+let wishButton = doccument.querySelector('.wish-btn');
+let itemsButton = doccument.querySelector('.item-btn');
+
+bagButton.addEventListener('click',()=>{
+  alert('bag clicked');
+})
+
+bagButton.addEventListener('click',()=>{
+  alert('wish clicked');
+})
+
+bagButton.addEventListener('click',()=>{
+  alert('item clicked');
+})
